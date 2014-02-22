ab46f5e1b1c12cf76ffa897e71068de28fa40b21
Robert Roach <rjayroach@gmail.com>
2014-02-22 09:11:24 +0800

    mods for cap 3

--- Rakefile
+++ Rakefile
@@ -1,1 +1,5 @@
 require "bundler/gem_tasks"
+require 'geminabox/rake'
+
+Geminabox::Rake.install namespace: 'gib', dir: '.', host: ENV['GEM_SERVER_MAXCOLE']
+
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'ngems'
+set :repo_url, 'git@github.com:rjayroach/gembox.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, "/srv/prod/apps/#{fetch(:application)}"
 
 # Default value for :scm is :git
 # set :scm, :git
